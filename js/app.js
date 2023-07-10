053101e06d57cb13fc24492cf9c849da4e5b7a65
Hamed <hamedpyn@gmail.com>
2023-07-11 03:13:59 +0430

    completing the project

--- js/app.js
+++ js/app.js
@@ -1,11 +1,50 @@
-let $ = document ,
-emailInput = $.querySelector('#email'),
-subjectInput = $.querySelector('#subject'),
-messageInput = $.querySelector('#message'),
-sendBtn = $.querySelector('#sendBtn'),
-resetBtn = $.querySelector('#resetBtn')
+let $ = document,
+    emailInput = $.querySelector('#email'),
+    subjectInput = $.querySelector('#subject'),
+    messageInput = $.querySelector('#message'),
+    sendBtn = $.querySelector('#sendBtn'),
+    resetBtn = $.querySelector('#resetBtn'),
+    form = $.querySelector('form'),
+    spinner = $.querySelector('#spinner')
 
-function disablingSendBtn(){
-    sendBtn.style.disabled
+// events
+
+emailInput.addEventListener('blur', formValidation)
+subjectInput.addEventListener('blur', formValidation)
+messageInput.addEventListener('blur', formValidation)
+sendBtn.addEventListener('click', loaders)
+$.addEventListener('DOMContentLoaded', () => {
+    disablingSendBtn()
+})
+resetBtn.addEventListener('click', () => {
+    form.reset()
+})
+function disablingSendBtn() {
+    sendBtn.disabled = true
 }
-disablingSendBtn()
+
+function formValidation() {
+    if (this.value.length >= 1) {
+        if (this.type == 'email') {
+            if (this.value.includes('@')) {
+                this.style.borderBottomColor = 'green'
+            } else {
+                this.style.borderBottomColor = 'red'
+            }
+        } else {
+            this.style.borderBottomColor = 'green'
+        }
+    } else {
+        this.style.borderBottomColor = 'red'
+    }
+    if (subjectInput.style.borderBottomColor == 'green' && emailInput.style.borderBottomColor == 'green' && messageInput.style.borderBottomColor == 'green') {
+        sendBtn.disabled = false
+    }
+}
+
+function loaders(){
+    spinner.setAttribute('style','display:block')
+    setTimeout(()=>{
+        spinner.setAttribute('style','display:none')
+    },7000)
+}
